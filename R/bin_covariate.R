838751cfd84db34db2bf7e535a6633114cc82bb6
thauffe <torsten.hauffe@gmail.com>
2019-10-22 09:49:45 +0200

    Fix covariate scaling

--- R/bin_covariate.R
+++ R/bin_covariate.R
@@ -6,15 +6,17 @@ bin_covariate <- function(TimeSim,
   # Simulation in msm goes forward in time by increase of time
   # In DES, time is given in units before present
   Cov <- Cov[order(Cov[, 1], decreasing = TRUE), ]
-  Cov[, 1] <- max(Cov[, 1]) - Cov[, 1]
-  Cov[, 2] <- Cov[, 2] - Cov[nrow(Cov), 2]
   MaxTimeSim <- max(TimeSim)
   if (MaxTimeSim > max(Cov[, 1]))
   {
     stop("Time of simulation exceeds the one of the covariate")
   }
-  Cov <- Cov[Cov[,1] <= MaxTimeSim, ]
+  Cov <- Cov[Cov[, 1] <= MaxTimeSim, ]
+  Cov[, 1] <- max(Cov[, 1]) - Cov[, 1]
+  CovApp <- approx(Cov, xout = sort(unique(c(TimeSim, Cov[, 1]))), rule = 2 )
+  Cov <- data.frame(Age = CovApp$x, Cov = CovApp$y)
   ToBin <- data.frame(Cov, Bin = findInterval(Cov[,1], TimeSim))
   BinnedCov <- aggregate(ToBin[, 2], by = list(ToBin$Bin), FUN = mean)$x
+  BinnedCov <- BinnedCov - BinnedCov[length(BinnedCov)]
   return(BinnedCov)
 }
